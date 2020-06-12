64671251adefb9c265f0a399b68ddf55f02aeb30
noodles69 <66779548+noodles69@users.noreply.github.com>
2020-06-12 03:48:40 -0500

    yes
    why not

---
+++ commands/unban.js
@@ -0,0 +1,21 @@
+const Discord = require("discord.js");
+
+module.exports = {
+  name: "unban",
+  category: "Fun",
+  description: "Unban the naughty people!",
+  cooldown: 5,
+  guildOnly: true,
+  execute(bot, message, args) {   
+
+    const author = message.author;
+    const reciever = message.mentions.users.first();
+    if (!reciever) return message.channel.send(":x: | You need someone to unban!");
+ 
+    const demootEmbed = new Discord.MessageEmbed()
+    .setDescription(`:x: Invalid ``<user>`` argument. User cannot be un-banned./n /nUsage:/n``un-ban <user> <reason>`)
+    .setColor('RED')
+    message.channel.send(demootEmbed)
+    message.delete().catch(err => console.log(err));//use this if youre gonna delete a message
+  }
+};
