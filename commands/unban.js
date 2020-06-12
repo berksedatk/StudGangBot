9e76b17522ce1724a713bb8ac279f7916d4206d7
noodles69 <66779548+noodles69@users.noreply.github.com>
2020-06-12 03:50:29 -0500

    Update unban.js
    wrong thingies

--- commands/unban.js
+++ commands/unban.js
@@ -13,7 +13,7 @@ module.exports = {
     if (!reciever) return message.channel.send(":x: | You need someone to unban!");
  
     const demootEmbed = new Discord.MessageEmbed()
-    .setDescription(`:x: Invalid ``<user>`` argument. User cannot be un-banned./n /nUsage:/n``un-ban <user> <reason>`)
+    .setDescription(':x: Invalid ``<user>`` argument. User cannot be un-banned./n /nUsage:/n``un-ban <user> <reason>')
     .setColor('RED')
     message.channel.send(demootEmbed)
     message.delete().catch(err => console.log(err));//use this if youre gonna delete a message
