0b1036a400c2697637f819f2ebc002b93afb8e1d
noodles69 <66779548+noodles69@users.noreply.github.com>
2020-06-30 01:13:24 -0500

    Update index.js

--- index.js
+++ index.js
@@ -221,7 +221,7 @@ bot.on("message", msg => {
   if (has === false && msg.content.toLowerCase().includes("send") && msg.content.toLowerCase().includes("nude")) return msg.reply("It seems like you are looking for NSFW(nude) channels, I suggest you to check out the <#721635790760706079> channel for more info.");
   if (has === false && msg.content.toLowerCase().includes("i") && msg.content.toLowerCase().includes("nude")) return msg.reply("It seems like you are looking for NSFW(nude) channels, I suggest you to check out the <#721635790760706079> channel for more info.");
  
- //Attachment limiter
+ //Attachment limit
   if (msg.attachments.size >= 2 && !exception.includes(msg.channel.id)) {
     msg.delete({reason: "Multiple Attachments"})
     return msg.reply("Please upload **one** attachment per message.")
