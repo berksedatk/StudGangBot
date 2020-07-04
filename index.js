158afa4eb6bcc27a4746c2d348727473a19b8702
noodles69 <66779548+noodles69@users.noreply.github.com>
2020-07-04 02:55:04 -0500

    Update index.js

--- index.js
+++ index.js
@@ -85,11 +85,11 @@ bot.on('ready', () => {
   })
 
 
-  let msgchannel = ["556929328672145430"]
+  let msgchannel = ["673272861900800030"]
 
   msgchannel.forEach(channelid => {
     setInterval(function() {
-    bot.channels.cache.get(channelid).send({embed:  {description: 'Make a suggestion for it to posted in <#622983734144139295> so everyone can vote on your server ideas!\n Keep in mind that not all suggestions will happen even if you have a lot of checkmarks!\nSilly suggestions can get you wanred for wrong channel usage!\n**Command Usage** !suggest <suggestion>'}})
+    bot.channels.cache.get(channelid).send({embed:  {description: 'Simpsei'}})
   }, 8.64e+7)
   })
 
