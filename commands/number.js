961456d7e9dfb9be2b72234ed355f91b741fd9fa
noodles69 <66779548+noodles69@users.noreply.github.com>
2020-07-21 12:28:43 -0500

    Create number.js

---
+++ commands/number.js
@@ -0,0 +1,14 @@
+const Discord = require("discord.js");
+
+module.exports = {
+  name: "number",
+  category: "Fun",
+  description: "Let the bot pick a number 1-100 for you!",
+  aliases: ["pick","random","num"],
+  cooldown: 5,
+  guildOnly: true,
+  execute(bot, message, args) {
+let randomnumber = Math.floor(Math.random() * 100).toLocaleString()
+    message.channel.send(`${randomnumber}`); 
+  }
+};
