04d68426c83a1d3de1ff69763a4460f2e48ef46c
noodles69 <66779548+noodles69@users.noreply.github.com>
2020-07-21 12:31:47 -0500

    Update number.js

--- commands/number.js
+++ commands/number.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports = {
   name: "number",
-  category: "Fun",
+  category: "Utility",
   description: "Let the bot pick a number 1-100 for you!",
   aliases: ["pick","random","num"],
   cooldown: 5,
