2584b41178b5eeec7d185c2603136c33cb67ec0a
noodles69 <66779548+noodles69@users.noreply.github.com>
2020-07-22 01:44:57 -0500

    Update guildinfo.js

--- commands/guildinfo.js
+++ commands/guildinfo.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports = {
   name: "guildinfo",
-  category: "Moderation",
+  category: "Utility",
   description: "Lists the information about the guild you're in.",
   aliases: ["serverinfo"],
   cooldown: 5,
