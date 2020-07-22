1cf9df3457ca305854b5e9b4f1e3fece49f53761
noodles69 <66779548+noodles69@users.noreply.github.com>
2020-07-22 01:47:12 -0500

    Update snipe.js

--- commands/snipe.js
+++ commands/snipe.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports = {
   name: "snipe",
-  category: "Utility",
+  category: "Misc",
   description: "Snipe those deleted messages",
   cooldown: 5,
   guildOnly: true,
