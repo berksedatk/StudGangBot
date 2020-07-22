b024d4cd47b037484dc20ffe6a31a7611330d8e1
noodles69 <66779548+noodles69@users.noreply.github.com>
2020-07-22 01:48:05 -0500

    Update editsnipe.js

--- commands/editsnipe.js
+++ commands/editsnipe.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports = {
   name: "editsnipe",
-  category: "Utility",
+  category: "Misc",
   description: "Snipe those edited messages",
   cooldown: 5,
   guildOnly: true,
