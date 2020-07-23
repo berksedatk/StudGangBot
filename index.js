33ceebdd135a716f1c55e2c72b7b9ed9ab21a3b8
noodles69 <66779548+noodles69@users.noreply.github.com>
2020-07-23 01:43:26 -0500

    Update index.js

--- index.js
+++ index.js
@@ -206,7 +206,7 @@ bot.on("message", msg => {
   if (msg.author.bot) return;
 
   if (msg.member) {
-    if (msg.member.hasPermission("MANAGE_SERVER")) return;
+    if (msg.member.hasPermission("MANAGE_GUILD")) return;
   }
 
   //Blacklisted words
