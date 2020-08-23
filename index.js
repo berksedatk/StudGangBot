8765604bfcae48eaaf4b6080b8de49edd035ada4
noodles69 <66779548+noodles69@users.noreply.github.com>
2020-08-23 01:53:18 -0500

    Update index.js

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ bot.on('ready', () => {
 
   msgchannels.forEach(channelid => {
     setInterval(function() {
-    bot.channels.cache.get(channelid).send({embed: {description: "Always feel free to check out our main chat <#548949556210040862>!"}})
+    bot.channels.cache.get(channelid).send({embed: {description: "Always feel free to check out our main chat <#548949556210040862>! :)"}})
   }, 1800000)
   })
 
