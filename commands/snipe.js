294eddd7d2b261e40a41dd5da05ebd67db85f0a9
noodles69 <66779548+noodles69@users.noreply.github.com>
2020-08-25 22:04:29 -0500

    Update snipe.js

--- commands/snipe.js
+++ commands/snipe.js
@@ -12,7 +12,7 @@ module.exports = {
     if (!msg) return message.channel.send('Theres nothing to snipe!');
 
     const blacklist = ["730795824006692865","534349263422095370","474231127163404289","474231127163404289","642589313988558897",'194666207440273408','495248175808905226','401873000053342208','478721209224265748','230084136868642818']//if youre seeing this ur seci :flooshed: - no u - NO U - k
-    const filter = ["fag","chris","niggеr","negro","n¡gg3r","n¡gger","rape","rap3","r4pe","r4p3","nigger","retard","faggot","n i g g e r","kkk","k k k","beaner","b e a n e r","be a ner","b e a ner","be an er","be an e r",
+    const filter = ["niggеr","nigga","n1gg@","n1gga","fag","chris","niggеr","negro","n¡gg3r","n¡gger","rape","rap3","r4pe","r4p3","nigger","retard","faggot","n i g g e r","kkk","k k k","beaner","b e a n e r","be a ner","b e a ner","be an er","be an e r",
       'niggar','niggair','wetback','w e t b a c k','wet ba ck','we tback','w e tback','wet bac k','niggèr','rëtard','rétard','retarted','retarded',
       'r.e.t.a.r.d','r3tard','nigg3rs','nigg3r','retar.d','n1gg3r','nlgger','queer','qu e e r','que e r','qu ee r','q u e e r','q-e-e-r','quuer','quer','fags']
     const regex = /(https?:\/\/[^\s]+)/g
