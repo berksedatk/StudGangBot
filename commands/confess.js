e7b54fab57cd5bb22d131c1fbd46365468f396c9
noodles69 <66779548+noodles69@users.noreply.github.com>
2020-08-29 21:40:47 -0500

    Update confess.js

--- commands/confess.js
+++ commands/confess.js
@@ -13,17 +13,22 @@ module.exports = {
     const filter = ["Niggеr","nigga","n1gg@","n1gga","negro","n¡gg3r","n¡gger","fag","nigger","retard","faggot","n i g g e r","k k k","beaner","b e a n e r","be a ner","b e a ner","be an er","be an e r",
         'niggar','niggair','wetback','w e t b a c k','wet ba ck','we tback','w e tback','wet bac k','niggèr','rëtard','rétard','retarted','retarded',
         'r.e.t.a.r.d','r3tard','nigg3rs','nigg3r','retar.d','n1gg3r','nlgger','queer','qu e e r','que e r','qu ee r','q u e e r','q-e-e-r','quuer','quer','fags']
- 
+
     const titleList = [
       "As I heard from a Anonymous person",
       "A Anonymous person is confessing this",
       "Here is the Anonymous person's truth",
       "They were too shy to reveal it but here is Anonymous person's confession"
     ]
-        let confession = args.join(" ")
+
+    if (!args[0]) return message.channel.send(":x: | You need a message to put in confession.")
+
+    let confession = args.join(" ")
+
     filter.forEach(word => {
       if (confession.includes(word)) return message.channel.send(":x: | You can't use a blacklisted word in your message.")
-    
+    })
+
     let confessEmbed = new Discord.MessageEmbed()
     .setTitle(titleList[Math.floor(Math.random() * titleList.length)])
     .setColor("#efefef")
