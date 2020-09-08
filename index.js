48f35b50f8f3b99c5ba62bb3583fcfede1b44385
noodles69 <66779548+noodles69@users.noreply.github.com>
2020-09-07 20:22:30 -0500

    Update index.js

--- index.js
+++ index.js
@@ -218,7 +218,7 @@ bot.on("message", msg => {
 'we tback','w e tback','rëtard','rétard','retarted','retarded',
 'r.e.t.a.r.d','r3tard','nigg3rs','nigg3r','retar.d','n1gg3r','nlgger','queer','qu e e r','que e r','qu ee r',
 'q u e e r','qeer','quuer','quer','fags','chink','coon','dike','niggar','niggur','nigor','nigre','nigar','niggah','nig','nigguh','Niglet','nigglet','Nigra','negra','niggra',
-'nigrah','nigruh','chinki','chinky','Spic','spick','spik','spig','spigotty','whitey','nazi','Wigger','Wigga','wegro','nikker','nicker','icker','nikker','nikka','nibbah','nibba','nicka',
+'nigrah','nigruh','chinki','chinky','Spic','spick','spik','spig','dike','spigotty','whitey','nazi','Wigger','Wigga','wegro','nikker','nicker','icker','nikker','nikka','nibbah','nibba','nicka',
 'jews','jews','f a g g o t','f a g','queer','q u e e r','qu e e r','quee r','q u e er','quee er','quee e r','qu ee r','quee r','q ueer','queer','q ueer','q uee r','rape','rapist','molest','molester',
 'molestar','nibb','fagot','faget','c o o n','co o n','c o on','coo n','c oon']
 
