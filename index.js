5d26eec689fd7951b088c3f1392a214f145ce0f8
noodles69 <66779548+noodles69@users.noreply.github.com>
2020-09-07 19:26:18 -0500

    Update index.js

--- index.js
+++ index.js
@@ -213,11 +213,11 @@ bot.on("message", msg => {
 
   //Blacklisted words
   const exception = ["556927352513363968"]
-  const filter = ["niggеr",'n1gg3r','n1gger','n1gga','n!gger','n!gga','n!gg3r','nlgga','nlgger','nlgg3r','n1gg3r','n1gg@',"nigga","n1gg@","n1gga","negro","n¡gg3r","n¡gger","fag","nigger",
+  const filter = ["niggеr",'n1gg3r','n1gger','n1gga','n!gger','n!gga','n!gg3r','nlgga','nlgger','nlgg3r','n1gg3r','n1gg@',"n1gg@","n1gga","negro","n¡gg3r","fag",
 "retard","faggot","n i g g e r","k k k","beaner","b e a n e r","be a ner","b e a ner","be an er","be an e r",'niggar','niggair','wetback','w e t b a c k','wet ba ck',
-'we tback','w e tback','wetback','niggèr','rëtard','rétard','retarted','retarded',
+'we tback','w e tback','wetback','rëtard','rétard','retarted','retarded',
 'r.e.t.a.r.d','r3tard','nigg3rs','nigg3r','retar.d','n1gg3r','nlgger','queer','qu e e r','que e r','qu ee r',
-'q u e e r','qeer','quuer','quer','fags','chink','coon','dike','niggar','niggur','niger','nigor','nigre','nigar','niggah','nig','nigguh','Niglet','nigglet','Nigra','negra','niggra',
+'q u e e r','qeer','quuer','quer','fags','chink','coon','dike','niggar','niggur','nigor','nigre','nigar','niggah','nig','nigguh','Niglet','nigglet','Nigra','negra','niggra',
 'nigrah','nigruh','chinki','chinky','Spic','spick','spik','spig','spigotty','whitey','nazi','Wigger','Wigga','wegro','nikker','nicker','icker','nikker','nikka','nibbah','nibba','nicka',
 'jews','jews','f a g g o t','f a g','queer','q u e e r','qu e e r','quee r','q u e er','quee er','quee e r','qu ee r','quee r','q ueer','queer','q ueer','q uee r','rape','rapist','molest','molester',
 'molestar','nibb','nig','nigg','fagot','faget','c o o n','co o n','c o on','coo n','c oon']
