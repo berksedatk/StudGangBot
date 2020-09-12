0fac50029cff934cd7e1a85fb095426f0ef06cc8
noodles69 <66779548+noodles69@users.noreply.github.com>
2020-09-12 15:10:51 -0500

    Create suggest.js

---
+++ commands/suggest.js
@@ -0,0 +1,14 @@
+const Discord = require("discord.js");
+
+module.exports = {
+  name: "suggest",
+  category: "Utility",
+  description: "Suggestion information command for StudGang",
+  cooldown: 420,
+  reqPermissions: ["MANAGE_GUILD"],
+  guildOnly: true,
+  execute(bot, message, args) {
+    message.channel.send('Make a suggestion for it to posted in <#622983734144139295> so everyone can vote on your server ideas!\n Keep in mind that not all suggestions will happen even if you have a lot of checkmarks!\nSilly suggestions can get you warned for wrong channel usage!\n**Command Usage** !suggest <suggestion>')
+   message.delete().catch(O_o=>{}); 
+  }
+};
