755c3e5a3dce686eee590ff0acd4a64f019cdbe5
noodles69 <66779548+noodles69@users.noreply.github.com>
2020-09-16 03:02:16 -0500

    Update seggs.js

--- commands/seggs.js
+++ commands/seggs.js
@@ -1,32 +1,27 @@
 const Discord = require("discord.js");
 
 module.exports = {
-  name: "basement",
+  name: "seggs",
   category: "Fun",
-  description: "Why just ask them while you can force?",
-  aliases: ["kidnap"],
+  description: "It's seggs.. 😐",
+  aliases: ["segg"],
   usage: "[mention]",
   cooldown: 5,
   guildOnly: true,
   execute(bot, message, args) {
     const author = message.author;
     const reciever = message.mentions.users.first();
-    if (!reciever) return message.channel.send(":x: | You need someone to kidnap... Unless you're a fucking weirdo");
+    if (!reciever) return message.channel.send(":x: | You need someone to have seggs... Unless you're a fucking weirdo");
 
     const images = [
-      'https://cdn.discordapp.com/attachments/576647339263918092/700020223222480976/56bed0b0de5405a3f6915f1dc9c9b904.gif',
-      'https://cdn.discordapp.com/attachments/576647339263918092/700020222526095431/situational-awareness-zero.gif',
-      'https://cdn.discordapp.com/attachments/576647339263918092/700020221938892880/e2d.gif',
-      'https://cdn.discordapp.com/attachments/576647339263918092/700020221548953651/tenor.gif',
-      'https://cdn.discordapp.com/attachments/576647339263918092/700020219904524428/tenor_2.gif',
-      'https://cdn.discordapp.com/attachments/576647339263918092/700020218742833169/tenor_3.gif',
-      "https://cdn.discordapp.com/attachments/548949556210040862/704436488905162923/image0.jpg"
+      'https://cdn.discordapp.com/emojis/721286508971163710.gif',
+      'https://cdn.discordapp.com/emojis/722538829512310884.gif'
     ]
     
     const image = images[Math.floor(Math.random() * images.length)];
   
     const basementEmbed = new Discord.MessageEmbed()
-    .setDescription(`${message.mentions.users.first()} *gets carried to* ${message.author}*'s basement quietly...*`)
+    .setDescription(`${message.mentions.users.first()} and ${message.author} start having seggs!`)
     .setImage(image)
     .setColor('RANDOM')
     message.channel.send(basementEmbed)
