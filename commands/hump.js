0d5e532cc4268b6a19180c84f5103884ac0f25f6
noodles69 <66779548+noodles69@users.noreply.github.com>
2020-09-16 05:18:41 -0500

    Update hump.js

--- commands/hump.js
+++ commands/hump.js
@@ -15,7 +15,7 @@ module.exports = {
     if (!reciever) return message.channel.send(":x: | You need someone to have dry-hump... Unless you're a fricking weirdo");
 
     const images = [
-      'https://cdn.discordapp.com/emojis/546559461330780352.gif",
+      'https://cdn.discordapp.com/emojis/546559461330780352.gif'
     ]
     
     const image = images[Math.floor(Math.random() * images.length)];
