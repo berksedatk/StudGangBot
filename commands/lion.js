c589cd543f7677e2800ddb5b182a2ae4ff737be3
noodles69 <66779548+noodles69@users.noreply.github.com>
2020-09-18 04:37:15 -0500

    Update lion.js

--- commands/lion.js
+++ commands/lion.js
@@ -9,6 +9,5 @@ module.exports = {
   guildOnly: true,
   execute(bot, message, args) {
     message.channel.send("Lion is a old ass boomer ex-mod but is pretty nice and has good memes.")
-   message.delete().catch(O_o=>{}); 
   }
 };
