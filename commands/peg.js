1a0748a3ed928c6be8cd73443e8ce909d91ecb4d
noodles69 <66779548+noodles69@users.noreply.github.com>
2020-09-24 03:27:38 -0500

    Create peg.js

---
+++ commands/peg.js
@@ -0,0 +1,12 @@
+
+const Discord = require("discord.js");
+
+module.exports = {
+  name: "spank",
+  category: "Fun",
+  description: "Spank Someone.. :flushed:",
+  usage: "[mention]",
+  cooldown: 5,
+  guildOnly: true,
+  execute(bot, message, args) {
+  }
