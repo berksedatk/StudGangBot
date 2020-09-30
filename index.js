3fd7fb4e68a90acc7f384c7c9f8401113b3a203a
noodles69 <66779548+noodles69@users.noreply.github.com>
2020-09-30 18:27:53 -0500

    Update index.js

--- index.js
+++ index.js
@@ -224,7 +224,7 @@ bot.on("message", msg => {
   const exception = ["556927352513363968"]
   const filter = ['nigger','n1gg3r','nigga','n1gger','n1gga','n!gger','n!gga','n!gg3r','nlgga','nlgger','nlgg3r','n1gg3r',
 "n¡gg3r",'fag','retard','faggot',"n i g g e r",'beaner',"b e a n e r","be a ner","b e a ner","be an er","be an e r",
-'niggair','wetback','w e t b a c k','wet ba ck','we tback','w e tback','rëtard','rétard','retarted','retarded',
+'niggair','wetback','w e t b a c k','wet ba ck','we tback','w e tback','rëtard','rétard','retarted',
 'r.e.t.a.r.d','r3tard','nigg3rs','nigg3r','retar.d','n1gg3r','nlgger','fags','chink','niggar','niggur','nigor',
 'nigre','nigar','niggah','nigguh','Niglet','nigglet','Nigra','negra','niggra','nigrah','nigruh','chinki','chinky',
 'spick','spig','spigotty','whitey','nazi','Wigger','Wigga','wegro','nikker','nikker','nikka','nibbah',
