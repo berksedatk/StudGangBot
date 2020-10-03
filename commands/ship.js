5ce794651f5650f2d222b286bc4e2630dd1384a9
SaxDev <berksedat123@gmail.com>
2020-10-03 23:04:20 +0300

    g

--- commands/ship.js
+++ commands/ship.js
@@ -6,44 +6,43 @@ module.exports = {
   description: "Ship yourself or someone else to see how good of a couple it makes! /n**Custom Command**: kia.#0002",
   usage: "<user> [user]",
   cooldown: 5,
-  execute(bot, message, args) {
+  async execute(bot, message, args) {
     if (!args[0]) return message.channel.send(":x: | You need something or someone to ship yourself with!")
 
-    let randomnumber = Math.floor(Math.random() * 100)
-    console.log(randomnumber)
+    let number = Math.floor(Math.random() * 100)
     let loadingbar = ""
     let custommsg = ""
-    if (randomnumber = 0) {
+    if (number == 0) {
       loadingbar = ":black_large_square::black_large_square::black_large_square::black_large_square::black_large_square::black_large_square::black_large_square::black_large_square::black_large_square::black_large_square:"
       custommsg = "No love at all! :broken_heart:"
-    } else if (randomnumber < 11) {
+    } else if (number < 11) {
       loadingbar = ":red_square::black_large_square::black_large_square::black_large_square::black_large_square::black_large_square::black_large_square::black_large_square::black_large_square::black_large_square:"
       custommsg = "Not a match! :person_gesturing_no:"
-    } else if (randomnumber < 21) {
+    } else if (number < 21) {
       loadingbar = ":red_square::red_square::black_large_square::black_large_square::black_large_square::black_large_square::black_large_square::black_large_square::black_large_square::black_large_square:"
       custommsg = "Maybe a small hope. :crying_cat_face:"
-    } else if (randomnumber < 31) {
+    } else if (number < 31) {
       loadingbar = ":red_square::red_square::red_square::black_large_square::black_large_square::black_large_square::black_large_square::black_large_square::black_large_square::black_large_square:"
       custommsg = "Not too much :no_mouth:"
-    } else if (randomnumber < 41) {
+    } else if (number < 41) {
       loadingbar = ":red_square::red_square::red_square::red_square::black_large_square::black_large_square::black_large_square::black_large_square::black_large_square::black_large_square:"
       custommsg = "It has potential :thinking:"
-    } else if (randomnumber < 51) {
+    } else if (number < 51) {
       loadingbar = ":red_square::red_square::red_square::red_square::red_square::black_large_square::black_large_square::black_large_square::black_large_square::black_large_square:"
       custommsg = "Might be a good match! :clap:"
-    } else if (randomnumber < 61) {
+    } else if (number < 61) {
       loadingbar = ":red_square::red_square::red_square::red_square::red_square::red_square::black_large_square::black_large_square::black_large_square::black_large_square:"
       custommsg = "Cute together uwu :eyes:"
-    } else if (randomnumber < 71) {
+    } else if (number < 71) {
       loadingbar = ":red_square::red_square::red_square::red_square::red_square::red_square::red_square::black_large_square::black_large_square::black_large_square:"
       custommsg = "More likely :yum:"
-    } else if (randomnumber < 81) {
+    } else if (number < 81) {
       loadingbar = ":red_square::red_square::red_square::red_square::red_square::red_square::red_square::red_square::black_large_square::black_large_square:"
       custommsg = "Adorable together :smirk_cat:"
-    } else if (randomnumber < 91) {
+    } else if (number < 91) {
       loadingbar = ":red_square::red_square::red_square::red_square::red_square::red_square::red_square::red_square::red_square::black_large_square:"
       custommsg = "Wholesome! :smiling_face_with_3_hearts:"
-    } else if (randomnumber = 100) {
+    } else if (number == 100) {
       loadingbar = ":red_square::red_square::red_square::red_square::red_square::red_square::red_square::red_square::red_square::red_square:"
       custommsg = "A match!!! :heart_eyes:"
     }
@@ -52,7 +51,7 @@ module.exports = {
       let shipEmbed = new Discord.MessageEmbed()
       .setColor("PINK")
       .setTimestamp()
-      .setTitle("❤️**Ship Meter**❤️")
+      .setTitle("❤️ **Ship Meter** ❤️")
       .setFooter(`Requested by: ${message.author.tag}`, message.author.avatarURL())
       .setDescription(`${args[0]} and ${args[1]}\n${randomnumber}% - ${custommsg}\n ${loadingbar}`)
       message.channel.send(shipEmbed)
@@ -60,7 +59,7 @@ module.exports = {
       let shipEmbed = new Discord.MessageEmbed()
       .setColor("PINK")
       .setTimestamp()
-      .setTitle("❤️**Ship Meter**❤️")
+      .setTitle("❤️ **Ship Meter** ❤️")
       .setFooter(`Requested by: ${message.author.tag}`, message.author.avatarURL())
       .setDescription(`${args[0]} and ${message.author}\n${randomnumber}% - ${custommsg}\n ${loadingbar}`)
       message.channel.send(shipEmbed)
