f2ce02fdb2b8376447caed8100bfc3e59af41f3e
noodles69 <66779548+noodles69@users.noreply.github.com>
2020-10-02 19:51:29 -0500

    Update water-remind.js

--- commands/water-remind.js
+++ commands/water-remind.js
@@ -1,1 +1,20 @@
+const Discord = require("discord.js");
 
+module.exports = {
+  name: "water-remind",
+  category: "Fun",
+  description: "Remind someone to drink water!",
+  aliases: "remind",
+  usage: "[mention]",
+  cooldown: 5,
+  guildOnly: true,
+  execute(bot, message, args) {    
+ const author = message.author;
+    const reciever = message.mentions.users.first();
+    if (!reciever) return message.channel.send(":x: | You need to tag someone to remind them to drink water!");
+    const slapEmbed = new Discord.MessageEmbed()
+    .setDescription(`**${reciever}**, please remember to drink water today if you haven't done so already!`)
+    .setColor('#699DEB')
+    message.channel.send(slapEmbed)
+  }
+};
