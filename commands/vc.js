7e0bad8e1670902c6ffe0e4424e67e2f808223c2
noodles69 <66779548+noodles69@users.noreply.github.com>
2020-10-07 18:48:05 -0500

    Update vc.js

--- commands/vc.js
+++ commands/vc.js
@@ -1,14 +1,1 @@
-const Discord = require("discord.js");
 
-module.exports = {
-  name: "vc",
-  category: "Utility",
-  unstaged: true,
-  dev: true,
-  description: "Bot joins noodles VC",
-  aliases: ["join"],
-  cooldown: 5,
-  execute(bot, message, args) {
-  
-     message.guild.channels.cache.get("591123171504029696").join()
-  }
