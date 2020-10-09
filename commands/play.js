3665a534de8161056aa61a6c4e82a9d4266e7432
SaxDev <berksedat123@gmail.com>
2020-10-09 19:46:26 +0300

    gay

--- commands/play.js
+++ commands/play.js
@@ -51,23 +51,23 @@ module.exports = {
      return message.channel.send(`${song.title} has been added to the queue!`);
     }
   }
-};
 
-function play(guild, song) {
-  const serverQueue = bot.queue.get(guild.id);
-  if (!song) {
-    serverQueue.voiceChannel.leave();
-    bot.queue.delete(guild.id);
-    return;
+  function play(guild, song) {
+    const serverQueue = bot.queue.get(guild.id);
+    if (!song) {
+      serverQueue.voiceChannel.leave();
+      bot.queue.delete(guild.id);
+      return;
+    }
+
+    const dispatcher = serverQueue.connection
+      .play(ytdl(song.url))
+      .on("finish", () => {
+        serverQueue.songs.shift();
+        play(guild, serverQueue.songs[0]);
+      })
+      .on("error", error => console.error(error));
+    dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
+    serverQueue.textChannel.send(`Start playing: **${song.title}**`);
   }
-
-  const dispatcher = serverQueue.connection
-    .play(ytdl(song.url))
-    .on("finish", () => {
-      serverQueue.songs.shift();
-      play(guild, serverQueue.songs[0]);
-    })
-    .on("error", error => console.error(error));
-  dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
-  serverQueue.textChannel.send(`Start playing: **${song.title}**`);
-}
+};
