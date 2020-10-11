883eac00c4dad7d59a0018f31469c1d16fb8685c
noodles69 <66779548+noodles69@users.noreply.github.com>
2020-10-10 21:17:03 -0500

    Update updatemods.js

--- commands/updatemods.js
+++ commands/updatemods.js
@@ -14,7 +14,7 @@ module.exports = {
     
     const eventTotal = modlist.events.head.length + modlist.events.senior.length + modlist.events.junior.length + modlist.events.trial.length + modlist.events.assist.length
     
-    const partnerTotal = modlist.partner.managers.length  + (" - 50+ Total PM's")
+    const partnerTotal = modlist.partner.managers.length  + (" <:SG_dash:764672634080067604> 50+ Total PM's")
     
 
     const moderationTeam = new Discord.MessageEmbed()
