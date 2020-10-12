d621e4aebf3147c6e90cb4c5e28a66bb149b36d2
noodles69 <66779548+noodles69@users.noreply.github.com>
2020-10-12 01:08:55 -0500

    Create rules.js

---
+++ commands/rules.js
@@ -0,0 +1,23 @@
+
+const Discord = require("discord.js");
+
+module.exports = {
+  name: "rules",
+  category: "Utility",
+  description: "",
+  aliases: "rs",
+  reqPermissions: ["ADMINISTRATOR"],
+  cooldown: 5,
+  unstaged: true,
+  vip: "true",
+  guildOnly: true,
+  execute(bot, message, args) {    
+ const author = message.author;
+    const rulesEmbed = new Discord.MessageEmbed()
+    .setTitle("**__Server Rules__**
+    .setDescription(`Thank you for joining StudGang, now we have some rules in place to ensure a safe environment for everyone here, if you notice people breaking rules, please message our [modmailbot ping] in the top right of your sidebar. \n`)
+    .setColor('RED')
+    message.channel.send(rulesEmbed)
+   message.delete(); 
+  }
+};
