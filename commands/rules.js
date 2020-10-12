4ca2bb95b63beb1e9c028bf4f652dfbb5274d638
noodles69 <66779548+noodles69@users.noreply.github.com>
2020-10-12 18:14:04 -0500

    Update rules.js

--- commands/rules.js
+++ commands/rules.js
@@ -14,8 +14,26 @@ module.exports = {
   execute(bot, message, args) {    
     const rulesEmbed = new Discord.MessageEmbed()
     .setTitle("**__Server Rules__**")
-    .setDescription(`Thank you for joining StudGang, now we have some rules in place to ensure a safe environment for everyone here, if you notice people breaking rules, please message our [modmailbot ping] in the top right of your sidebar. \n`)
+    .setDescription(' \n ')
     .setColor('#1fbceb')
-    message.channel.send(rulesEmbed)
+
+    
+        const inforulesEmbed = new Discord.MessageEmbed()
+    .setTitle("**__Server Rules__**")
+    .setDescription(` /n**__Follow the Discord ToS and Community Guidelines__** /n
+1) We are highly strict when it comes to the rules Discord themselves have set out. 100% of the time you will be banned for violating those rules. /n
+ 
+    •  https://discordapp.com/tos /n
+    •  https://discordapp.com/guidelines /n
+**__Do not attempt to find loopholes around our rules nor bypass them.__** /n
+ 
+ 2) Bypassing or finding loopholes around the rules is not taken lightly. An example would be creating an alt to get out of a mute on your main account. (mute/temp-ban) /n`)
+    .setColor('#f53348')
+
+    
+      message.delete().catch(O_o=>{}); 
+          message.channel.send(inforulesEmbed).then(() => {
+              message.channel.send(rulesEmbed)   
+    
     }
 };
