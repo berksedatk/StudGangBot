7e522625a3594d066ed2235f334884043aceaf03
noodles69 <66779548+noodles69@users.noreply.github.com>
2020-10-12 19:03:58 -0500

    Update rules.js

--- commands/rules.js
+++ commands/rules.js
@@ -14,20 +14,20 @@ module.exports = {
   execute(bot, message, args) {    
     const rulesEmbed = new Discord.MessageEmbed()
     .setTitle("**__Server Rules__**")
-    .setDescription(' \n ')
+    .setDescription(` \n**__Follow the Discord ToS and Community Guidelines__** \n
+1) We are highly strict when it comes to the rules Discord themselves have set out. 100% of the time you will be banned for violating those rules. \n
+ 
+    •  https://discordapp.com/tos \n
+    •  https://discordapp.com/guidelines \n
+**__Do not attempt to find loopholes around our rules nor bypass them.__** \n
+ 
+ 2) Bypassing or finding loopholes around the rules is not taken lightly. An example would be creating an alt to get out of a mute on your main account. (mute/temp-ban) \n`)
     .setColor('#1fbceb')
 
     
         const inforulesEmbed = new Discord.MessageEmbed()
-    .setTitle("**__Server Rules__**")
-    .setDescription(` /n**__Follow the Discord ToS and Community Guidelines__** /n
-1) We are highly strict when it comes to the rules Discord themselves have set out. 100% of the time you will be banned for violating those rules. /n
- 
-    •  https://discordapp.com/tos /n
-    •  https://discordapp.com/guidelines /n
-**__Do not attempt to find loopholes around our rules nor bypass them.__** /n
- 
- 2) Bypassing or finding loopholes around the rules is not taken lightly. An example would be creating an alt to get out of a mute on your main account. (mute/temp-ban) /n`)
+    .setTitle("**__Server Information__**")
+    .setDescription('thanks for touching my penis we have rules so u dont be bad lol xd lmao')
     .setColor('#f53348')
 
         message.delete().catch(O_o=>{}); 
