3bee5a0049a8074dbbb349ff267d028b9b46c970
noodles69 <66779548+noodles69@users.noreply.github.com>
2020-10-12 19:05:01 -0500

    Update rules.js

--- commands/rules.js
+++ commands/rules.js
@@ -14,14 +14,12 @@ module.exports = {
   execute(bot, message, args) {    
     const rulesEmbed = new Discord.MessageEmbed()
     .setTitle("**__Server Rules__**")
-    .setDescription(` \n**__Follow the Discord ToS and Community Guidelines__** \n
-1) We are highly strict when it comes to the rules Discord themselves have set out. 100% of the time you will be banned for violating those rules. \n
- 
+    .setDescription(` \n**__Follow the Discord ToS and Community Guidelines__**
+1) We are highly strict when it comes to the rules Discord themselves have set out. 100% of the time you will be banned for violating those rules.
     •  https://discordapp.com/tos \n
     •  https://discordapp.com/guidelines \n
-**__Do not attempt to find loopholes around our rules nor bypass them.__** \n
- 
- 2) Bypassing or finding loopholes around the rules is not taken lightly. An example would be creating an alt to get out of a mute on your main account. (mute/temp-ban) \n`)
+**__Do not attempt to find loopholes around our rules nor bypass them.__**
+  2) Bypassing or finding loopholes around the rules is not taken lightly. An example would be creating an alt to get out of a mute on your main account. (mute/temp-ban) \n`)
     .setColor('#1fbceb')
 
     
