09d7e4174a338373af512542dc910904e2b6cdda
noodles69 <66779548+noodles69@users.noreply.github.com>
2020-10-13 21:13:28 -0500

    Update refresh.js

--- commands/refresh.js
+++ commands/refresh.js
@@ -12,12 +12,12 @@ module.exports = {
     if (!args[0]) return message.channel.send(`<:cross:724049024943915209> | Missing arguments. \n-Usage: \`${message.prefix}${this.name} ${this.usage}\``);
     let commandName = args[0].toLowerCase()
     if (!bot.commands.get(commandName)) return message.channel.send(" <:cross:724049024943915209> | This command does not exist.");
-    let category = bot.commands.get(commandName)
+
 
     try {
-      delete require.cache[require.resolve(`../${category}/${commandName}.js`)];
+      delete require.cache[require.resolve(`../commands/${commandName}.js`)];
       bot.commands.delete(commandName);
-      const pull = require(`../${category}/${commandName}.js`);
+      const pull = require(`../commands/${commandName}.js`);
       bot.commands.set(commandName, pull);
     } catch(err) {
       return message.channel.send("<:cross:724049024943915209> | Could not reload the command: " + err);
