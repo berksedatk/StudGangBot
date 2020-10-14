3919b03b388ed4c969eef505d5301d4d129c0554
noodles69 <66779548+noodles69@users.noreply.github.com>
2020-10-13 21:16:45 -0500

    Update demote.js

--- commands/demote.js
+++ commands/demote.js
@@ -13,7 +13,7 @@ module.exports = {
     if (!reciever) return message.channel.send(":x: | You need someone to demote!");
 
     const demootEmbed = new Discord.MessageEmbed()
-    .setDescription(`${reciever} has been **DEMOTED** from professional r word to stupid ass Member by ${author}!`)
+    .setDescription(`yo this works??!`)
     .setColor('RED')
     message.channel.send(demootEmbed)
     message.delete().catch(err => console.log(err));//use this if youre gonna delete a message
