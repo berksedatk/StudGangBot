5443c20d7d53dd5fad3c5399e03a792eea556a16
noodles69 <66779548+noodles69@users.noreply.github.com>
2020-10-15 02:43:15 -0500

    Create ppmilk.js

---
+++ commands/ppmilk.js
@@ -0,0 +1,16 @@
+const Discord = require("discord.js");
+
+module.exports = {
+  name: "L",
+  category: "Fun",
+  description: "Give people all the L's!",
+  aliases: "l",
+  usage: "[mention]",
+  cooldown: 5,
+  guildOnly: true,
+  execute(bot, message, args) {
+  
+  if (message.content === 'pp-milk') {
+	message.react('722539935898271784');
+}
+};
