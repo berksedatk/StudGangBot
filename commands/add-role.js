7fbe6c446c6112a45265f6096054e51b832e831c
noodles69 <66779548+noodles69@users.noreply.github.com>
2020-10-15 19:33:42 -0500

    Create add-role.js

---
+++ commands/add-role.js
@@ -0,0 +1,39 @@
+const Discord = require("discord.js");
+
+module.exports = {
+  name: "add-role",
+  category: "Utility",
+  description: "Add a role to a member",
+  aliases: ["tests"],
+  reqPermissions: "MANAGE_ROLES",
+  usage: "[role]",
+  cooldown: 5,
+  guildOnly: true,
+  execute(bot, message, args) {
+  
+  const checkPermissionRole = (role) => role.permissions.has('ADMINISTRATOR')
+       console.log(`args have been added to {$message.author}`)
+       let { cache } = message.guild.roles;
+       let role = cache.find(find => role.name.toLowerCase() === args);
+       if (role) {
+            if (message.member.roles.cache.has(role.id)) {
+             message.channel.send(":x: You already have that role!");
+             return;
+       }
+          if(checkPermissionRole(role)) {
+              message.channel.send(":x: You probably don't want to give someone a role that equals your own! (Administrator)");
+              }
+              else {
+                  message.member.roles.add(role)
+                     .then(member => message.channel.send("You were added to this role"))
+                     .catch(err => {
+                      console.log(err));
+                      message.channel.send("Something went wrong. . .");
+                  });
+        }
+       else {
+            message.channel.send(":x: That is not a valid role or I could not find it!")
+       }
+
+  }
+};
