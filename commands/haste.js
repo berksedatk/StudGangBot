53e4b17d626482808518549f69f345e5194c8171
noodles69 <66779548+noodles69@users.noreply.github.com>
2020-10-16 19:52:52 -0500

    Update haste.js

--- commands/haste.js
+++ commands/haste.js
@@ -16,7 +16,8 @@ module.exports = {
           let output = code;
           hastebin(output, { extention: 'txt' }).then(code => {
         const embed = new Discord.MessageEmbed()
-          .setDescription(`Arguments have been uploaded to Pastebin! LOL You can find it [here](code)`)
+          .setTitle(`Arguments have been uploaded to Pastebin!`)
+          .setURL(code)
           .setColor("#02ff00");
           msg.edit(embed);
           });
