9104efdcc3d03f8b819f3ae1c9c1ab81bf86ec17
noodles69 <66779548+noodles69@users.noreply.github.com>
2020-10-16 20:02:12 -0500

    Update haste.js

--- commands/haste.js
+++ commands/haste.js
@@ -10,16 +10,16 @@ module.exports = {
   usage: "[text]",
   dev: "true",
   async execute(bot, message, args) {
-      const msg = await message.channel.send(`Uploading to Hastebin...`);
-   if (!args[0]) return message.channel.send(":x: | You need to provide text for it to be uploaded to Hastebin.");
+     const msg = await message.channel.send(`Uploading to Hastebin...`);
+          if (!args[0]) return message.channel.send(":x: | You need to provide text for it to be uploaded to Hastebin.");
     const code = args.join(" ");
           let output = code;
           hastebin(output, { extention: 'txt' }).then(code => {
-        const embed = new Discord.MessageEmbed()
+    const embed = new Discord.MessageEmbed()
           .setTitle(`Arguments have been uploaded to Hastebin!`)
           .setURL(code)
           .setColor("#02ff00");
           msg.edit(embed);
-          });
-      }
+        });
+     }
  };
