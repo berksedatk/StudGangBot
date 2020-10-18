50a0f2f4ab3a741c8f9d898e646de97e21b5822f
noodles69 <66779548+noodles69@users.noreply.github.com>
2020-10-17 21:55:16 -0500

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,73 +1,110 @@
 const Discord = require("discord.js");
-const fs = require('fs');
+const config = require("../config.json");
 
 module.exports = {
   name: "help",
   category: "General",
-  description: "Shows the list of commands.",
+  description: "Commands of the bot.",
   aliases: ["commands"],
   usage: "[command name]",
   cooldown: 5,
   execute(bot, message, args) {
-    const categories = fs.readdirSync('./commands').filter(file => !file.endsWith('.js'));
-    const misc = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-
+    const general = [];
+    const fun = [];
+    const moderation = [];
+    const utility = [];
+    const misc = [];
+    const music = [];
+    const { commands } = message.client;
     if (!args[0]) {
-
-      let helpEmbed = new Discord.MessageEmbed()
-      .setTitle("Here are the commands:")
-      .setDescription(`Use \`${message.prefix}help <command name>\` to get into a command's details.`)
-      .setTimestamp()
-      .setFooter("Requested by " + message.author.username, message.author.avatarURL())
-      .setColor("BLUE");
-
-      categories.forEach(category => {
-        if (category === "dev") return;
-
-        let helpList = [];
-        const commands = fs.readdirSync(`./commands/${category}`).filter(file => file.endsWith('.js'));
-
-        commands.forEach(command => {
-          command = command.slice(0, command.length-3)
-          if (!bot.commands.get(command).dev && !bot.commands.get(command).unstaged) helpList.push(`\`${command}\``);
-        });
-
-        const categoryName = category.charAt(0).toUpperCase() + category.slice(1);
-
-        helpEmbed.addField(categoryName, helpList.join(", "));
+      commands.map(c => {
+        if (c.dev || c.admin || c.unstaged) {
+          return;
+        } else if (c.category === "General") {
+          general.push(`\`${c.name}\``);
+        } else if (c.category === "Music") {
+          music.push(`\`${c.name}\``);
+        } else if (c.category === "Fun") {
+          fun.push(`\`${c.name}\``);
+        } else if (c.category === "Moderation") {
+          moderation.push(`\`${c.name}\``);
+        } else if (c.category === "Utility") {
+          utility.push(`\`${c.name}\``);
+        } else {
+          misc.push(`\`${c.name}\``);
+        }
       });
 
-      if (misc.length < 0) {
-        let miscList = []
-        misc.forEach(c => {
-          if (!bot.commands.get(command).dev && !bot.commands.get(command).unstaged) miscList.push(`\`${command}\``)
+      let helpEmbed = new Discord.MessageEmbed()
+        .setTitle("Here are the commands:")
+        .setDescription(`You can provide a command to get into details. ${config.prefix}help <command name>`)
+        .setTimestamp()
+        .setFooter("Requested by " + message.author.username,message.author.avatarURL())
+        .setColor("999999");
+      if (general[0]) helpEmbed.addField("General Commands:", general.join(", "));
+      if (music[0]) helpEmbed.addField("Music Commands:", music.join(", "));
+      if (fun[0]) helpEmbed.addField("🎉 Fun Commands:", fun.join(", "));
+      if (moderation[0]) helpEmbed.addField("🛠️ Moderation Commands:", moderation.join(", "));
+      if (utility[0]) helpEmbed.addField("🔧 Utility Commands:", utility.join(", "));
+      if (misc[0]) helpEmbed.addField("Misc. Commands:", misc.join(", "));
+      return message.author.send(helpEmbed).then(() => {
+          if (message.channel.type === "dm") return;
+          message.react("📩");
         })
-        helpEmbed.push("Misc.", miscList.join(", "))
+        .catch(error => {
+          message.reply("It seems like I cant DM you. Please enable your DMs!");
+        });
+    } else if (args[0]) {
+      const name = args[0].toLowerCase();
+      const command = commands.get(name) || commands.find(c => c.aliases && c.aliases.includes(name));
+
+      if (!command) {
+        return message.reply(`This command doesn't exists! use ${config.prefix}help to get into commands.`);
       }
 
-      message.channel.send(helpEmbed);
-    } else {
-
-      let command = bot.commands.get(args[0].toLowerCase())
-      if (!command) return message.channel.send(`<:cross:724049024943915209> | There's no such a command like that, to see the full command list please use \`${message.prefix}commands\` command. `);
-
       let helpEmbed = new Discord.MessageEmbed()
-      .setTitle(command.name)
-      .setDescription('The usage in <> is required, [] is optional.')
-      .setFooter("Requested by " + message.author.tag, message.author.avatarURL())
-      .setColor("#ffffff")
-      if (command.description) helpEmbed.addField("**Description**", command.description);
-      if (command.category) helpEmbed.addField("**Category**", command.category)
-      if (command.aliases) helpEmbed.addField("**Aliases**", command.aliases.join(', '));
-      if (command.usage) helpEmbed.addField("**Usage**", message.prefix + command.name + " " + command.usage);
-      if (command.examples) helpEmbed.addField("**Examples**", command.examples);
-      if (command.reqPermissions) helpEmbed.addField("**Required Permission(s)**", command.reqPermissions.join(', '));
-      if (command.guildOnly) helpEmbed.addField("**Guild Only**", "Command only can be executed in a server.");
-      if (command.dmOnly) helpEmbed.addField("**DM Only**", "Command only can be executed in DMs.");
-      if (command.dev) helpEmbed.addField("**Dev**", "Only Bot Developers can execute this command.");
-      if (command.voted) helpEmbed.addField("**Voted**", "You have to vote for the bot to use this command.")
-      if (command.cooldown) helpEmbed.addField("**Cooldown**", command.cooldown + " second(s)");
-      message.channel.send(helpEmbed)
+        .setTitle(command.name)
+        .setColor("BLUE")
+        .setTimestamp()
+        .setFooter(
+          "Requested by " + message.author.tag,
+          message.author.avatarURL()
+        );
+
+      if (command.aliases)
+        helpEmbed.addField("**Aliases:**", command.aliases.join(", "));
+      if (command.category)
+        helpEmbed.addField("**Category:**", command.category);
+      if (command.description)
+        helpEmbed.addField("**Description:**", command.description);
+      if (command.usage)
+        helpEmbed.addField(
+          "**Usage:**",
+          config.prefix + command.name + " " + command.usage
+        );
+      if (command.reqPermissions)
+        helpEmbed.addField(
+          "**Required Permission(s):**",
+          command.reqPermissions.join(", ")
+        );
+      if (command.guildOnly)
+        helpEmbed.addField(
+          "**Guild Only**",
+          "Command only can be executed in a guild."
+        );
+      if (command.dev)
+        helpEmbed.addField(
+          "**Dev Only**",
+          "Only bot owner can execute this command."
+        );
+      if (command.admin)
+        helpEmbed.addField(
+          "**Admin**",
+          "Only bot admins can execute this command."
+        );
+      if (command.cooldown)
+        helpEmbed.addField("**Cooldown:**", command.cooldown + " second(s)");
+      message.channel.send(helpEmbed);
     }
   }
 };
