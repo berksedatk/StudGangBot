23395716852a79de48ce5019c42341f1f672ecd3
noodles69 <66779548+noodles69@users.noreply.github.com>
2020-10-20 02:13:43 -0500

    Create setbotname.js

---
+++ commands/setbotname.js
@@ -0,0 +1,21 @@
+const Discord = require("discord.js");
+
+module.exports = {
+  name: "setbotname",
+  category: "Utility",
+  description: "Set's the bots username.",
+  usage: "<name>",
+  dev: true,
+  async execute(bot, message, args) {
+      const newName = message.content.split(' ');
+
+    try{
+        client.user.setUsername(newName[1])
+            .then(user => message.channel.send(`My new username is **${user.username}**`))
+            .catch(console.error);
+    }
+    catch(error){
+        message.channel.send("I could not set my new username :sob:");
+    }
+  }
+};
