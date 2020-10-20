b182e36e935ee5b9fc22cc892fdfd0cc5f1033a7
noodles69 <66779548+noodles69@users.noreply.github.com>
2020-10-20 01:58:56 -0500

    Update flipcoin.js

--- commands/flipcoin.js
+++ commands/flipcoin.js
@@ -1,10 +1,10 @@
 const Discord = require("discord.js");
 
 module.exports = {
-  name: "avatar",
+  name: "flipcoin",
   category: "Utility",
-  description: "Get a specified user's avatar.",
-  aliases: ["av"],
+  description: "Choose heads or tails and the bot will flip the coin!",
+  aliases: ["flip-coin","coinflip","coin-flip"],
   usage: "[heads] [tails]",
   cooldown: 60,
   async execute(bot, message, args) {
