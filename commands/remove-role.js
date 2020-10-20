aa84be8cf4fcfaf66e7a2cba56579ad27eedbdf9
noodles69 <66779548+noodles69@users.noreply.github.com>
2020-10-20 01:21:36 -0500

    Update remove-role.js

--- commands/remove-role.js
+++ commands/remove-role.js
@@ -11,38 +11,38 @@ module.exports = {
   async execute(bot, message, args) {
       let rMember = message.guild.member(message.mentions.users.first()) || message.guild.members.cache.get(args[0]);
 
-    if(!message.member.hasPermission("MANAGE_ROLES_OR_PERMISSIONS")){
+    if(!message.member.hasPermission("MANAGE_ROLES")){
         message.channel.send("");
     }
     
     else{
 
         if(!rMember) 
-            return message.channel.send("Couldn't find that user, yo.");
+            return message.channel.send("Couldn't find that user.");
         
         let role = args.join(" ").slice(23);
         if(!role) 
-            return message.channel.send("Specify a role!");
+            return message.channel.send("Please specofy a role.");
         
         let gRole = message.guild.roles.cache.find(roles => roles.name === role);
         if(!gRole) 
-            return message.channel.send("Couldn't find that role.");
+            return message.channel.send("I couldn't find that role.");
 
         if(!rMember.roles.cache.has(gRole.id)) 
-            return message.reply("They don't have that role.");
+            return message.reply("They do not have that role.");
         
         else{
             rMember.roles.remove(gRole.id).catch(console.error);
             
             try{
                 rMember.send(`Sorry, you lost the ${gRole.name} role`);
-                message.channel.send(`The user ${rMember} has lost the ${gRole.name} role`);
+                message.channel.send(`**${gRole.name}** has been removed from **${rMember}**`);
             }
             catch(e){
                 console.log(e.stack);
-                message.channel.send(`RIP to <@${rMember.id}>, We removed ${gRole.name} from them.`)
-            }
+                message.channel.send(`**${gRole.name}** has been removed from **${rMember}**`)
+           }
         }
-    }
+     }
   }
 };
