f7f138b75d35c5b1c4e2ad1d0518ee53d607901e
SaxDev <30241194+SaxDev@users.noreply.github.com>
2020-10-27 23:46:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const Discord = require("discord.js");
 const bot = new Discord.Client({
   disableMentions: "everyone",
   ws: {
-    intents: ["GUILD_MEMBERS"]
+    intents: ["GUILDS","GUILD_MEMBERS"]
   }
 });
 bot.snipes = new Map();
