34ad1f0fe1e81d7345ab0143a1943f4bb9c5e9e9
noodles69 <66779548+noodles69@users.noreply.github.com>
2020-10-28 04:40:14 -0500

    Update L.js

--- commands/L.js
+++ commands/L.js
@@ -8,6 +8,7 @@ module.exports = {
   usage: "[mention]",
   cooldown: 5,
   guildOnly: true,
+  blacklistedChannels: ["580083502867808287","606314770546556961","709248514550530048","730994932424245258","730682637051756554","635320134847954954","718972368416014367","689865780292223012"],
   execute(bot, message, args) {    
  const author = message.author;
     const reciever = message.mentions.users.first();
