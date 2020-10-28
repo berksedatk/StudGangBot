cde8909f8fc3e2cfc15cbde0c3eada0e018834ee
noodles69 <66779548+noodles69@users.noreply.github.com>
2020-10-28 05:44:18 -0500

    Update pp.js

--- commands/pp.js
+++ commands/pp.js
@@ -7,7 +7,7 @@ module.exports = {
   description: "Shows you your pp size.",
   cooldown: 15,
   execute(bot, message, args) {  
-    if message.author.id === 194666207440273408 return message.channel.send(":x: | This users pp is too big to fit into an embed.");
+    if (message.author.id === '194666207440273408') return message.channel.send(":x: | This users pp is too big to fit into an embed.");
     let ppsize = Math.floor(Math.random() * 25)
     let pp =  ""
     for (let i = 0; ppsize > i; i++) {
