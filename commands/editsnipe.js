4ee06a0c23fc1b3e1f99a3eca6f27e5c46e8ca11
noodles69 <66779548+noodles69@users.noreply.github.com>
2020-10-28 04:40:37 -0500

    Update editsnipe.js

--- commands/editsnipe.js
+++ commands/editsnipe.js
@@ -6,6 +6,7 @@ module.exports = {
   description: "Snipe those edited messages",
   cooldown: 5,
   guildOnly: true,
+  blacklistedChannels: ["580083502867808287","606314770546556961","709248514550530048","730994932424245258","730682637051756554","635320134847954954","718972368416014367","689865780292223012"],
   execute(bot, message, args) {
     const msg = bot.edits.get(message.channel.id)
     if (!msg) return message.channel.send('Theres nothing to editsnipe!');
