7baf550e0f16b3527f489eca805a920044490e85
noodles69 <66779548+noodles69@users.noreply.github.com>
2020-10-28 04:42:14 -0500

    Update win.js

--- commands/win.js
+++ commands/win.js
@@ -7,6 +7,7 @@ module.exports = {
   aliases: ["w"],
   cooldown: 5,
   guildOnly: true,
+  blacklistedChannels: ["580083502867808287","606314770546556961","709248514550530048","730994932424245258","730682637051756554","635320134847954954","718972368416014367","689865780292223012"],
   execute(bot, message, args) {    
     const slapEmbed = new Discord.MessageEmbed()
     .setDescription(`**${message.author.username}** see's this as an absolute win! :sunglasses:`)
