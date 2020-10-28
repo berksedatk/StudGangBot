3caba4c71f2986f0adbf09a8ec8c6aa9753f6d12
noodles69 <66779548+noodles69@users.noreply.github.com>
2020-10-28 05:40:21 -0500

    Update pp.js

--- commands/pp.js
+++ commands/pp.js
@@ -6,10 +6,7 @@ module.exports = {
   category: "Fun",
   description: "Shows you your pp size.",
   cooldown: 15,
-  execute(bot, message, args) {
-  if (config.owners.includes(message.author.id)) {
-    return message.reply(":x: | Thsi users pp is so big that I cannot fit it into an embed.").then(m => m.delete({timeout: 15000}))
-  }    
+  execute(bot, message, args) {  
     let ppsize = Math.floor(Math.random() * 25)
     let pp =  ""
     for (let i = 0; ppsize > i; i++) {
