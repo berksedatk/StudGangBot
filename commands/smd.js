bc649f8489b86c98009ac0eb22f0e6685eb8da03
noodles69 <66779548+noodles69@users.noreply.github.com>
2020-11-01 23:11:40 -0600

    Update smd.js

--- commands/smd.js
+++ commands/smd.js
@@ -7,6 +7,9 @@ module.exports = {
   aliases: ["smc"],
   cooldown: 5,
   guildOnly: true,
+  exchannels: [
+    {id: "548949556210040862", cooldown: 10}
+  ],
   execute(bot, message, args) {
     message.channel.send(`Starts giving ${message.author} that super duper extreme tongue swirl sloppy toppy super soaker quadrouple combo 360 hurricane vacuum seal double hand gawk gawk cum blaster 3000 V2`);
   }
