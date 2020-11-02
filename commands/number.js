41eb162b3ee2cd2fde4e972903312458ee75f70e
noodles69 <66779548+noodles69@users.noreply.github.com>
2020-11-01 23:05:33 -0600

    Update number.js

--- commands/number.js
+++ commands/number.js
@@ -7,6 +7,9 @@ module.exports = {
   aliases: ["pick","random","num"],
   cooldown: 5,
   guildOnly: true,
+  exchannels: [
+    {id: "548949556210040862", cooldown: 10}
+  ],
   execute(bot, message, args) {
 let randomnumber = Math.floor(Math.random() * 100).toLocaleString()
     message.channel.send(`${randomnumber}`); 
