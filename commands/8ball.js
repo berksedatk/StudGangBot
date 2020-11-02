cbfa72fe033e04f9f9ac23157b8b754283801743
noodles69 <66779548+noodles69@users.noreply.github.com>
2020-11-01 22:56:30 -0600

    Update 8ball.js

--- commands/8ball.js
+++ commands/8ball.js
@@ -7,6 +7,9 @@ module.exports = {
   usage: "<question>",
   cooldown: 5,
   guildOnly: true,
+  exchannels: [
+    {id: "548949556210040862", cooldown: 15}
+  ],
   execute(bot, message, args) {
 const responses = [
   "It is certain",
