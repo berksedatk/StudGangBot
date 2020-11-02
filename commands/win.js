b341a6cce3718343135417b0bf49b55721459b6f
noodles69 <66779548+noodles69@users.noreply.github.com>
2020-11-01 23:13:14 -0600

    Update win.js

--- commands/win.js
+++ commands/win.js
@@ -7,6 +7,9 @@ module.exports = {
   aliases: ["w"],
   cooldown: 5,
   guildOnly: true,
+  exchannels: [
+    {id: "548949556210040862", cooldown: 7}
+  ],
   blacklistedChannels: ["580083502867808287","606314770546556961","709248514550530048","730994932424245258","730682637051756554","635320134847954954","718972368416014367","689865780292223012"],
   execute(bot, message, args) {    
     const slapEmbed = new Discord.MessageEmbed()
