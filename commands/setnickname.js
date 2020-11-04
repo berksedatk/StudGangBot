b859acb7407ea3dc97dc372ce90d9ee473e56696
noodle <66779548+noodles69@users.noreply.github.com>
2020-11-04 04:03:09 -0600

    Update setnickname.js

--- commands/setnickname.js
+++ commands/setnickname.js
@@ -5,15 +5,18 @@ module.exports = {
   category: "General",
   aliases: ["setnick","setnickname"],
   description: "Set a users nickname via a command.",
+  usage: ["<mention> <nickname>"],
   reqPermissions: ['MANAGE_NICKNAMES'],
   cooldown: 5,
   execute(bot, message, args) {
     let logchannel = bot.channels.cache.get('563402253139050496');
-    let user = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
-    if(!user)
+    let member = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
+    const newname = args.slice(1).join(' ');
+    if(!member)
     return message.channel.send(":x: | Please mention a valid member of this server");
-        message.guild.members.get(user).setNickname("Command Executed");
-        message.channel.send(`Successfully changed ${user}'s nickname to "${message.channel.server.detailsOf(user).nick}"`);
-        logchannel.send({embed: {title: "User Nickname changed via command!", description:`Responsible Mod:\n ${message.author}\n Targeted User:\n ${user}\nNew Nickname:\n${message.channel.server.detailsOf(user).nick}`, color:'#f83e42'}})
+    if (!args[1]) return message.channel.send(":x: | You need to provide a new nickname!");
+        member.setNickname(newname);
+        message.channel.send(`Successfully changed ${member.user.tag}'s nickname to: ${newname}`);
+        logchannel.send({embed: {title: "User Nickname changed via command!", description:`Responsible Mod:\n ${message.author.tag}\n Targeted User:\n ${member.user.tag}\nNew Nickname:\n${newname}`, color:'#f83e42'}})
   }
 };
