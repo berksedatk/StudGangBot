f9a812c69f6179212e774c15af0cbed9400677fc
noodle <66779548+noodles69@users.noreply.github.com>
2020-11-20 20:06:24 -0600

    Update editsnipe.js

--- commands/editsnipe.js
+++ commands/editsnipe.js
@@ -30,8 +30,8 @@ module.exports = {
     
     let inp = false
     filter.forEach(word => {
-      if (msg.content.toLowerCase().includes(word)) inp = true
-    });
+      if (msg.content.includes(word)) inp = true
+    })
     if (regex.test(msg.content)) inp = true
     if (inp === true || msg.content.length > 250) {
       snipeEmbed.setDescription('~~Inappropriate Message~~')
