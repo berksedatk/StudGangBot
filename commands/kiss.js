b3b90b02094ebffaed5a0ce0ff810760861d6653
noodle <66779548+noodles69@users.noreply.github.com>
2020-12-13 00:53:55 -0600

    ehe

--- commands/kiss.js
+++ commands/kiss.js
@@ -17,26 +17,25 @@ module.exports = {
     if (!reciever) return message.channel.send(":x: | You need someone to kissy wissy, mention them!");
 
     const images = [
-      'https://cdn.discordapp.com/attachments/687865931938136109/700467616870826024/002kiss.gif',
-      'https://cdn.discordapp.com/attachments/687865931938136109/700468778651222126/002kiss2.gif',
-      'https://cdn.discordapp.com/attachments/687865931938136109/700469129727049749/animekiss.gif',
-      'https://cdn.discordapp.com/attachments/687865931938136109/700469420077613056/animekiss2.gif',
-      'https://cdn.discordapp.com/attachments/687865931938136109/700469786710376478/animekiss3.gif',
-      'https://cdn.discordapp.com/attachments/687865931938136109/700470171911061604/animekiss4.gif',
-      'https://cdn.discordapp.com/attachments/687865931938136109/700470630289637456/animekiss5.gif',
-      'https://cdn.discordapp.com/attachments/687865931938136109/700470969311035444/animekiss6.gif',
-      'https://cdn.discordapp.com/attachments/687865931938136109/700471793307353118/animekiss7.gif',
-      'https://cdn.discordapp.com/attachments/687865931938136109/700472565239513228/animekiss8.gif',
-      'https://cdn.discordapp.com/attachments/687865931938136109/700473665774419998/animekiss9.gif',
-      'https://cdn.discordapp.com/attachments/652681168994697236/700014025391472741/c2e2d217850896f642facdb2488943f7.gif',
-      'https://media.discordyui.net/reactions/kiss/I6cANgF.gif',
-      'https://media.discordyui.net/reactions/kiss/zAVG8ov.gif',
-      'https://media.discordyui.net/reactions/kiss/234082934.gif',
-      'https://media.discordyui.net/reactions/kiss/Xrxhow8.gif',
-      'https://media.discordyui.net/reactions/kiss/GpyA4H5.gif',
-      'https://media.discordyui.net/reactions/kiss/IaHHwD7.gif',
-      'https://media.discordyui.net/reactions/kiss/723805.gif',
-      'https://media.discordyui.net/reactions/kiss/I6cANgF.gif'
+'https://noodle.is-big.gay/syHaQs4AO',
+'https://noodle.is-big.gay/E1NkVqqbs.gif',
+'https://noodle.is-big.gay/UgljrnYw9.gif',
+'https://noodle.is-big.gay/RcCzqJ0oY.gif',
+'https://noodle.is-big.gay/cPqDTOEPX.gif',
+'https://noodle.is-big.gay/1xr0Mw7xt.gif',
+'https://noodle.is-big.gay/jULdWOYDT.gif',
+'https://noodle.is-big.gay/R0CdwDAFQ.gif',
+'https://noodle.is-big.gay/Jkusb3n0P.gif',
+'https://noodle.is-big.gay/QycnJG7Vt.gif',
+'https://noodle.is-big.gay/WPYWOryDV.gif',
+'https://noodle.is-big.gay/u2BpL-Pt1.gif',
+'https://noodle.is-big.gay/8Hvmv5DGf.gif',
+'https://noodle.is-big.gay/auoM6xJio.gif',
+'https://noodle.is-big.gay/RXF4WEuLJ.gif',
+'https://noodle.is-big.gay/7JKQcnw3F.gif',
+'https://noodle.is-big.gay/3kTKm4H_V.gif',
+'https://noodle.is-big.gay/-xQWYfDFI.gif',
+'https://noodle.is-big.gay/OH_p97po4.gif'
     ]
     
     const image = images[Math.floor(Math.random() * images.length)];
