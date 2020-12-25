196c609e39bd62b64de3504c3f5a6208c1f64c46
noodle <66779548+noodles69@users.noreply.github.com>
2020-12-24 18:57:54 -0600

    ee?

--- commands/roles.js
+++ commands/roles.js
@@ -14,34 +14,34 @@ module.exports = {
   execute(bot, message, args) {    
     const genderEmbed = new Discord.MessageEmbed()
     .setTitle("Age Roles")
-    .setDescription("
+    .setDescription(`
 :man: **Male
 :woman: Female
 :repeat: Transgender
-:no_entry_sign: Non-Binary**")
+:no_entry_sign: Non-Binary**`)
     .setFooter("React with the emote beside the role you want!")
     .setColor('#5E9BF7')
     
     const ageEmbed = new Discord.MessageEmbed()
     .setTitle("Age Roles")
-    .setDescription("
+    .setDescription(`
 :baby: **13-17
 :boy: 18-21
 :man: 22-29
-:older_man: 30+**")    
+:older_man: 30+**`)    
     .setFooter("React with the emote beside the role you want!")
     .setColor('#5E9BF7')
     
 
     const sexualityEmbed = new Discord.MessageEmbed()
     .setTitle("Sexuality Roles")
-    .setDescription("
+    .setDescription(`
 :kiss_woman_man: **Heterosexual
 :kiss_mm: Homosexual
 :kiss_ww: Lesbian
 :man_shrugging: Bisexual
 :woman_gesturing_no: Other(Ask-Me!)
-:smiling_imp: Homiesexual**")    
+:smiling_imp: Homiesexual**`)    
     .setFooter("React with the emote beside the role you want!")
     .setColor('#5E9BF7')
     
@@ -49,21 +49,21 @@ module.exports = {
 
     const pingEmbed = new Discord.MessageEmbed()
     .setTitle("Ping Roles")
-    .setDescription("
+    .setDescription(`
 :bangbang: **Announcements
 :tada: Giveaways
 :fire: Events
 :hammer_pick: Updates
 :handshake: Partnerships
-:smiling_imp: Ping Me Daddy~**")
+:smiling_imp: Ping Me Daddy~**`)
     .setFooter("React with the emote beside the role you want!")    
     .setColor('#5E9BF7')
     
 
     const gamingEmbed = new Discord.MessageEmbed()
     .setTitle("Gaming Channel Access")
-    .setDescription("
-:video_game: **Gaming**")    
+    .setDescription(`
+:video_game: **Gaming**`)    
     .setFooter("React with the emote to gain access to our Gaming Channels")
     .setColor('#5E9BF7')
     
