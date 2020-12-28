37d8a486400a399ba7f79b6513f8b75e69eea279
noodle <66779548+noodles69@users.noreply.github.com>
2020-12-28 01:25:16 -0600

    e

--- index.js
+++ index.js
@@ -70,12 +70,12 @@ bot.on('ready', () => {
 
 bot.on("guildCreate", guild => {
   // This event triggers when the bot joins a guild.
-  bot.channels.cache.get("792442072040079400").send({embed: {title: `Guild Joined`, description: `${guild.name} (id: ${guild.id}).\nOwner's ID: ${guild.ownerID} \nThis Guild has ${guild.memberCount} members.`, color: '5bff14'}});
+  bot.channels.cache.get("792442072040079400").send({embed: {title: `Guild Joined`, description: `${guild.name} (id: ${guild.id}).\nOwner's ID: ${guild.ownerID} \nThis Guild has ${guild.memberCount} members.`,  thumbnail: `https://cdn.discordapp.com/icons/${guild.id}/${guild.icon}.webp`, color: '5bff14'}});
 });
 
 bot.on("guildDelete", guild => {
   // this event triggers when the bot is removed from a guild.
-  bot.channels.cache.get("792442072040079400").send({embed: {title: 'Guild Left', description: `${guild.name} (id: ${guild.id})\nOwner's ID: ${guild.ownerID}\nThis Guild had ${guild.memberCount} members!`, color: 'ff0000'}});
+  bot.channels.cache.get("792442072040079400").send({embed: {title: 'Guild Left', description: `${guild.name} (id: ${guild.id})\nOwner's ID: ${guild.ownerID}\nThis Guild had ${guild.memberCount} members!`,thumbnail: `https://cdn.discordapp.com/icons/${guild.id}/${guild.icon}.webp`, thumbnail: `https://cdn.discordapp.com/icons/${guild.id}/${guild.icon}.webp`, color: 'ff0000'}});
 });
 
 //#On Message Deleted#
