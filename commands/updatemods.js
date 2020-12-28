cf671ea3ebc51a4d2573b0f266adb538e7df6287
noodle <66779548+noodles69@users.noreply.github.com>
2020-12-28 03:44:07 -0600

    nice

--- commands/updatemods.js
+++ commands/updatemods.js
@@ -15,7 +15,7 @@ module.exports = {
     
     const eventTotal = modlist.events.head.length + modlist.events.senior.length + modlist.events.junior.length + modlist.events.trial.length + modlist.events.assist.length
     
-    const partnerTotal = modlist.partner.managers.length + modlist.partner.head.length
+    const partnerTotal = modlist.partner.managers.length
     
 
     const moderationTeam = new Discord.MessageEmbed()
