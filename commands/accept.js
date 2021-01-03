1437fee2763bac916d1e6f127ac2a6f14c9be2a5
noodle <66779548+noodles69@users.noreply.github.com>
2021-01-02 21:27:21 -0600

    ee

--- commands/accept.js
+++ commands/accept.js
@@ -6,37 +6,28 @@ module.exports = {
   unstaged: true,
   description: "yea",
   usage: "[user]",
-  reqPermissions: ["MANAGE_GUILD"],
   cooldown: 3,
   sped: true,
+  dev: true,
   async execute(bot, message, args) {
     let rMember = message.guild.member(message.mentions.users.first()) || message.guild.members.cache.get(args[0]);
-    if(!message.member.hasPermission("MANAGE_GUILD")){
-        message.channel.send("Access Denied").then(m => m.delete({timeout: 2000}));
-    }
-
-    else{
-        let role1 = message.member.guild.roles.cache.find(role => role.name === `Helper`);
-        let role2 = message.member.guild.roles.cache.find(role => role.name === `-Server Staff`);      
-
         if(!rMember) 
             return message.channel.send(":x: | I couldn't find that user.").then(m => m.delete({timeout: 5000}))
         
         else{
-            rMember.roles.add(role1).catch(console.error);
-            rMember.roles.add(role2).catch(console.error);
-            message.delete();
            const success = new Discord.MessageEmbed()
 
             .setTitle("Hooray!")
-            .setDescription(`${rMember} has been successfully accepted into the StudGang Staff Team by ${message.author.tag}`)
+            .setDescription(`${rMember} has been successfully accepted into the Support Team by ${message.author.tag}`)
             .setColor('#42f12c')
             .setTimestamp()
             message.channel.send(success)
 
             const acceptance = new Discord.MessageEmbed()
-            .setTitle("Your Helper Application Has Been Accepted!")
-            .setDescription(`:tada: We are welcoming you with open arms into our Moderation team as one of our newest Helpers! :tada:\n It is recommended to start off your journey by checking out <#694971157916680282> to insure that you have a great experience with your time with us.\nIf you have any questions/concerns, please so say so in <#745783788596101210> where you will be able to talk with other staff members one on one. `)
+            .setTitle("Your Application Has Been Accepted By An Admin!")
+            .setDescription(`:tada: We are welcoming you with open arms into our Support Team as one of our newest Trial-Support Team Member! :tada:\nAlthough you do not receive any roles in the main server, we have a seperate support guild with all the information and rules needed to get started!`)
+            .addField("Permanent Support Server Invite:","[Click Here!](https://discord.gg/WGMrkqhuYb)")
+            .addField("Questions/Concerns?","Ask noodle anything! He will try his best to give you the answer you need")
             .setColor('#42f12c')
             .setTimestamp()
             rMember.send(acceptance)
