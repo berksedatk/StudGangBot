8be1a60225d9dfbfe95ab4b0134fa841824fa5c0
SaxDev <30241194+SaxDev@users.noreply.github.com>
2021-01-04 23:07:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -149,9 +149,6 @@ bot.on('message', message => {
    if (command.vip && !config.vip.includes(message.author.id)) {
     return message.reply(":x: | You are not allowed to use this command!");
   }
-   if (command.sped && !config.speds.includes(message.author.id)) {
-    return message.reply(":x: | You are not allowed to use this command!");
- }
     if (command.mnc && !config.mnc.includes(message.author.id)) {
     return message.reply(":x: | You are not cool enough to use this command!");
   }
