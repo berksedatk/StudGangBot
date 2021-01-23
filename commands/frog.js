3aba9de417ca89fc7a966546f62e2a1f52c47b0e
noodle <66779548+noodles69@users.noreply.github.com>
2021-01-23 06:09:39 -0600

    yuh

--- commands/frog.js
+++ commands/frog.js
@@ -1,32 +1,31 @@
-const Discord = require("discord.js");
-const cheerio = require('cheerio');
-const request = require('request');
-module.exports = {
-  name: "phrog",
-  category: "Fun",
-  description: "Get phrog pics",
-  cooldown: 20,
-  guildOnly: true,
-  aliases: ["frog"],
-  async execute(bot, message, args) {
-    const msg = await message.channel.send(`Generating Phrog Pics...`);
+phatforg = "http://results.dogpile.com/serp?qc=images&q=" + "fat frogs"
+    const choices = [
+      forg,
+      lollmeme,
+      phorg,
+      cutiefrogs,
+      thiccforgs,
+      babieforgs,
+      phatforg
+      ]
+    const pick = Math.floor(Math.random() * choices.length);
      Image(message);
     function Image(message){
     const options = {
-    url: "http://results.dogpile.com/serp?qc=images&q=" + "frog",
+    url: choices[pick],
     method: "GET",
     headers: {
     "Accept": "text/html",
     "User-Agent": "Chrome"
       }
     };
-      
+
     request(options, function(error, response, responseBody) {
 
     if (error) {
          return;
     }
-     
+    
      
     $ = cheerio.load(responseBody);
      
