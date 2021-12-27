77658dc9cc0ff0836a36887bbf0607e20b4ad372
noodle <66779548+noodles69@users.noreply.github.com>
2021-12-27 02:21:00 -0600

    Bored Cleaning

--- commands/haste.js
+++ commands/haste.js
@@ -15,7 +15,7 @@ module.exports = {
           if (!args[0]) return message.channel.send(":x: | You need to provide text for it to be uploaded to Hastebin.");
     const code = args.join(" ");
           let output = code;
-          hastebin(output, { extention: 'txt' }).then(code => {
+          hastebin(output, { url: "https://paste.hep.gg" , extention: 'txt' }).then(code => {
     const embed = new Discord.MessageEmbed()
           .setTitle(`Arguments have been uploaded to Hastebin!`)
           .setURL(code)
