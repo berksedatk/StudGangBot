37f3b9e3c4026c2f631e4cca5274450afea8a35b
noodle <66779548+noodles69@users.noreply.github.com>
2021-12-27 02:33:22 -0600

    Bored Cleaning

--- commands/changelog.js
+++ commands/changelog.js
@@ -11,7 +11,7 @@ module.exports = {
   blacklistedChannels: ["791317380521394186","791317317140217866","791319900550725643","791319939348561930","791318245718097971","791317348735909928","791317335695818752","791318209978302474"],
   execute(bot, message, args) {
     message.channel.send(`**Removed un-needed files:**\n> basement.txt, emote.txt, frog.txt, hump.txt. nitro.txt, noodles.txt, peg.txt, seggs.txt, sex.txt, ship.txt, spank.txt, spoon.txt, succ.txt\n
-**Removed Music Commands since they are broken and no one uses them.**\n> nowplaying.js, play.js, queue,js, skip.js, stop.js\note
-**Removed useless commands:**\n> react.js, roles.js\n**Removed un-used package:**\n>nhentai-js@3.3.0`)
+**Removed Music Commands since they are broken and no one uses them.**\n> nowplaying.js, play.js, queue,js, skip.js, stop.js
+**Removed useless commands:**\n> react.js, roles.js\n**Removed un-used package:**\n> nhentai-js@3.3.0`)
   }
 };
